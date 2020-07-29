af601c5052e97cb902fae7e0c61800b1635ee527
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-29 15:18:04 +0900

    Create template

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const https = require('https');
+const cron = require("node-cron");
+
+var options = {
+    hostname: 'typetalk.com',
+    path: '/api/v1/topics/xxxxxx',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-TYPETALK-TOKEN': 'xxxxxxxxxxxx'
+    }
+};
