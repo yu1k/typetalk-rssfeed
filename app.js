38735458c43d8f108f0d34dd065d06a54a760caa
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-29 15:37:30 +0900

    Add messageSend()

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ const parser = require("rss-parser");
 
 var options = {
     hostname: 'typetalk.com',
-    path: '/api/v1/topics/xxxxxx',
+    path: '/api/v1/topics/xxxxx',
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'X-TYPETALK-TOKEN': 'xxxxxxxxxxxx'
+        'X-TYPETALK-TOKEN': 'xxxxxxxxxxxxxxxxxx'
     }
 };
 
+function messageSend(){
+    var req = https.request(options, function (res){
+        console.log("STATUS: " + res.statusCode);
+        res.on("data", function(chunk){
+            console.log("BODY: " + chunk);
+        });
+    });
+    req.write(JSON.stringify({"message": "hoge"}));
+    req.end();
+}
+
 function main(){
-    return;
+    messageSend();
+    console.log("botを起動しました。");
 }
 
 main();
