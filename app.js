c5c593e5b0268de5104fa117e579e4b97504a26e
yu1k <48705423+yu1k@users.noreply.github.com>
2020-08-01 15:39:38 +0900

    Update

--- app.js
+++ app.js
@@ -2,31 +2,49 @@
 
 const https = require("https");
 const cron = require("node-cron");
-const parser = require("rss-parser");
+const Parser = require("rss-parser");
+let parser = new Parser();
 
 var options = {
     hostname: 'typetalk.com',
-    path: '/api/v1/topics/xxxxx',
+    path: '/api/v1/topics/xxxxxx',
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
-        'X-TYPETALK-TOKEN': 'xxxxxxxxxxxxxxxxxx'
+        'X-TYPETALK-TOKEN': 'xxxxxxxxxxxxxxxxxxxxx'
     }
 };
 
 function messageSend(){
+    //HTTPS
     var req = https.request(options, function (res){
         console.log("STATUS: " + res.statusCode);
         res.on("data", function(chunk){
             console.log("BODY: " + chunk);
         });
     });
-    req.write(JSON.stringify({"message": "hoge"}));
-    req.end();
-}
 
+    //RSSフィードの処理
+    (async () => {
+        let feed = await parser.parseURL('リンク');
+        console.log(feed.title);
+        feed.items.forEach(item => {
+            req.on("error", function(err){
+                if(err){
+                    console.log("Error.");
+                    return;
+                }
+            });
+            req.write(JSON.stringify({"message": item.title + '\n' + item.link}));
+            req.end();
+        });
+    })();
+    
+}
 function main(){
-    messageSend();
+    cron.schedule("10 * * * * *", () => {
+        messageSend();
+    });
     console.log("botを起動しました。");
 }
 
